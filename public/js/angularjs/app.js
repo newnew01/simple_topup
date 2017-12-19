69d444ded8732cc165d963c1ef165dcd80cc8d8d
newnew01 <newnew01@gmail.com>
2017-12-20 01:40:37 +0700

    progress

---
+++ public/js/angularjs/app.js
@@ -0,0 +1,127 @@
+var app = angular.module('topupApp', ['ngSanitize'], function($interpolateProvider) {
+    $interpolateProvider.startSymbol('<%');
+    $interpolateProvider.endSymbol('%>');
+});
+
+
+
+app.controller('topupController', function($scope,$http) {
+
+    $scope.username = 'bmV3bmV3MDE=';
+    $scope.password = 'bmV3a2h1bmcwMQ=='
+    $scope.users = branch_name;
+    $scope.balance = '0';
+    $scope.orderid = '';
+    $scope.content_topup_status = "<img class='status_icon' src='/image/loading.gif'><h3>กำลังทำรายการ...</h3>";
+
+    $scope.timerCheckStatus = null;
+
+
+    $scope.reloadBalance = function () {
+        $http.post("http://topup.newphone-function.trade/api/balance",{'username':Encryption.decode($scope.username),'password':Encryption.decode($scope.password)})
+            .then(function(response) {
+                //alert(response.data.AMOUNT);
+                $scope.balance = response.data.AMOUNT;
+            });
+    }
+
+    $scope.testfunc = function () {
+        alert(123);
+    }
+
+    $scope.topupRefill = function (network,number,cash,users) {
+        var data = {
+            'username':Encryption.decode($scope.username),
+            'password':Encryption.decode($scope.password),
+            'network':network,
+            'number':number,
+            'cash':cash,
+            'users':users
+        };
+
+        $scope.showStatusLoading();
+        //alert(network+'|'+number+'|'+cash+'|'+users);
+        $('#modal-topup-status').modal('show');
+       // alert();
+        //$scope.showStatusSuccess('ทำรายการสำเร็จ');
+       // alert();
+        //$scope.showStatusError('ผิดพลาด');
+
+        //setInterval($scope.testfunc, 3000);
+
+
+        $http.post("http://topup.newphone-function.trade/api/topup_refill",data)
+            .then(function(response) {
+                //alert(response.data.AMOUNT);
+                $('#modal-topup-status').modal('show');
+                if(response.data.STATUS != 1){
+                    $scope.showStatusError(response.data.DETAIL);
+                }else{
+                    $scope.orderid = response.data.ORDERID;
+                    $scope.timerCheckStatus = setInterval($scope.checkTopupStatus, 5000);
+                }
+            });
+
+    }
+
+    $scope.checkTopupStatus = function () {
+        var data = {
+            'username':Encryption.decode($scope.username),
+            'password':Encryption.decode($scope.password),
+            'orderid':$scope.orderid
+        };
+        $http.post("http://topup.newphone-function.trade/api/topup_status",data)
+            .then(function(response) {
+                if(response.data.STATUS == 1){
+                    if(response.data.STATUS_REFILL == 1){
+                        $scope.showStatusSuccess('การทำรายการสำเร็จ');
+                        clearInterval($scope.timerCheckStatus);
+                        $scope.clearData();
+                        $scope.orderid = '';
+                        $scope.reloadBalance();
+                    }
+
+                    if(response.data.STATUS_CANCEL == 1){
+                        $scope.showStatusError('ผิดพลาด: กรุณาตรวจสอบเครือข่ายมือถือของท่าน')
+                        clearInterval($scope.timerCheckStatus);
+                        $scope.clearData();
+                        $scope.orderid = '';
+                        $scope.reloadBalance();
+                    }
+                }else {
+                    $scope.showStatusError(response.data.DETAIL)
+                    clearInterval($scope.timerCheckStatus);
+                    $scope.clearData();
+                    $scope.orderid = '';
+                    $scope.reloadBalance();
+                }
+
+            });
+    }
+
+    $scope.showStatusSuccess = function (msg) {
+        $scope.content_topup_status = "<img src='/image/success.png' class='status_icon'><h3 class='status_msg_success'>"+msg+"</h3>";
+    }
+
+    $scope.showStatusError = function (msg) {
+        $scope.content_topup_status = "<img src='/image/error.png' class='status_icon'><h3 class='status_msg_error'>"+msg+"</h3>";
+    }
+
+    $scope.showStatusLoading = function () {
+        $scope.content_topup_status = "<img src='/image/loading.gif' class='status_icon'><h3>กำลังทำรายการ...</h3>";
+    }
+
+    $scope.clearData = function () {
+        $scope.ais_number = '';
+        $scope.dtac_number = '';
+        $scope.true_number = '';
+        $scope.my_number = '';
+        $scope.imobile_number = '';
+        $scope.pengoin_number = '';
+    }
+
+
+
+
+    $scope.reloadBalance();
+});
---
+++ resources/views/topup.blade.php
@@ -0,0 +1,378 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>NEW PHONE | ระบบเติมเงิน</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="/bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="/bower_components/font-awesome/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="/bower_components/Ionicons/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="/dist/css/AdminLTE.min.css">
+  <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+  <link rel="stylesheet" href="/dist/css/skins/_all-skins.min.css">
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
+  <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular-sanitize.js"></script>
+
+  <script src="/js/angularjs/app.js"></script>
+
+  <!-- Google Font -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+
+  <style>
+    .status_icon{
+      max-height: 250px;
+    }
+
+    .status_msg_success{
+      color: green;
+    }
+
+    .status_msg_error{
+      color: red;
+    }
+  </style>
+</head>
+<!-- ADD THE CLASS layout-top-nav TO REMOVE THE SIDEBAR. -->
+<body class="hold-transition skin-blue layout-top-nav skin-black" ng-app="topupApp" ng-controller="topupController">
+<div class="wrapper">
+
+  <header class="main-header">
+    <nav class="navbar navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <img src="/logo/logo_sm_text_right.png" height="40px" style="margin-top:5px">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+            <i class="fa fa-bars"></i>
+          </button>
+        </div>
+
+        <!-- /.navbar-collapse -->
+        <!-- Navbar Right Menu -->
+        <div class="navbar-custom-menu">
+          <ul class="nav navbar-nav">
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                <span class="hidden-xs">ยอดเงินคงเหลือ: <% balance %> บาท</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <!-- /.navbar-custom-menu -->
+      </div>
+      <!-- /.container-fluid -->
+    </nav>
+  </header>
+  <!-- Full Width Column -->
+  <div class="content-wrapper">
+    <div class="container">
+      <!-- Content Header (Page header) -->
+      <section class="content-header">
+        <h1>
+          ระบบเติมเงิน
+          <small>เวอร์ชั่น 1.0</small>
+        </h1>
+        <ol class="breadcrumb">
+          <li class="active">{{$user->branch_name}}</li>
+        </ol>
+      </section>
+
+      <!-- Main content -->
+      <section class="content">
+        <div class="row">
+          <div class="col-md-6">
+            <!-- Form Element sizes -->
+            <div class="box box-success">
+              <div class="box-header with-border">
+                <h3 class="box-title">AIS</h3>
+              </div>
+              <div class="box-body">
+                <center><img src="/logo/ais_logo.jpg" width="200" height="80"></center>
+                <input class="form-control input-lg" type="text" placeholder="เบอร์โทร" ng-model="ais_number">
+                <br>
+                <select class="form-control" ng-model="ais_cash">
+                  <option value="">คลิกเลือกราคา</option>
+                  <option value="5">5 (ไม่มีส่วนลด,ยกเลิกไม่ได้)</option>
+                  <option value="10">10</option>
+                  <option value="20">20</option>
+                  <option value="30">30</option>
+                  <option value="40">40</option>
+                  <option value="50">50</option>
+                  <option value="60">60</option>
+                  <option value="70">70</option>
+                  <option value="80">80</option>
+                  <option value="90">90</option>
+                  <option value="100">100</option>
+                  <option value="150">150</option>
+                  <option value="200">200</option>
+                  <option value="300">300</option>
+                  <option value="350">350</option>
+                  <option value="400">400</option>
+                  <option value="500">500</option>
+                  <option value="800">800</option>
+                  <option value="1000">1000</option>
+                </select>
+                <br>
+                <button type="button" class="btn btn-default btn-block" ng-click="topupRefill(1,ais_number,ais_cash,users)">เติมเงิน</button>
+              </div>
+
+              <!-- /.box-body -->
+            </div>
+            <!-- /.box -->
+          </div>
+
+          <div class="col-md-6">
+            <!-- Form Element sizes -->
+            <div class="box box-primary">
+              <div class="box-header with-border">
+                <h3 class="box-title">DTAC</h3>
+              </div>
+              <div class="box-body">
+                <center><img src="/logo/dtac_logo.jpg" width="200" height="80"></center>
+                <input class="form-control input-lg" type="text" placeholder="เบอร์โทร" ng-model="dtac_number">
+                <br>
+                <select class="form-control" ng-model="dtac_cash">
+                  <option value="">คลิกเลือกราคา</option>
+                  <option value="10">10</option>
+                  <option value="20">20</option>
+                  <option value="30">30</option>
+                  <option value="40">40</option>
+                  <option value="50">50</option>
+                  <option value="60">60</option>
+                  <option value="100">100</option>
+                  <option value="200">200</option>
+                  <option value="300">300</option>
+                  <option value="500">500</option>
+                  <option value="800">800</option>
+                </select>
+                <br>
+                <button type="button" class="btn btn-default btn-block" ng-click="topupRefill(2,dtac_number,dtac_cash,users)">เติมเงิน</button>
+              </div>
+
+              <!-- /.box-body -->
+            </div>
+            <!-- /.box -->
+          </div>
+
+          <div class="col-md-6">
+            <!-- Form Element sizes -->
+            <div class="box box-danger">
+              <div class="box-header with-border">
+                <h3 class="box-title">TRUE-H</h3>
+              </div>
+              <div class="box-body">
+                <center><img src="/logo/true_h_logo.jpg" width="200" height="80"></center>
+                <input class="form-control input-lg" type="text" placeholder="เบอร์โทร" ng-model="true_number">
+                <br>
+                <select class="form-control" ng-model="true_cash">
+                  <option value="">คลิกเลือกราคา</option>
+                  <option value="10">10</option>
+                  <option value="20">20</option>
+                  <option value="30">30</option>
+                  <option value="40">40</option>
+                  <option value="50">50</option>
+                  <option value="60">60</option>
+                  <option value="70">70</option>
+                  <option value="80">80</option>
+                  <option value="90">90</option>
+                  <option value="100">100</option>
+                  <option value="150">150</option>
+                  <option value="200">200</option>
+                  <option value="250">250</option>
+                  <option value="300">300</option>
+                  <option value="350">350</option>
+                  <option value="400">400</option>
+                  <option value="450">450</option>
+                  <option value="500">500</option>
+                  <option value="800">800</option>
+                  <option value="1000">1000</option>
+                </select>
+                <br>
+                <button type="button" class="btn btn-default btn-block" ng-click="topupRefill(3,true_number,true_cash,users)">เติมเงิน</button>
+              </div>
+
+              <!-- /.box-body -->
+            </div>
+            <!-- /.box -->
+          </div>
+
+          <div class="col-md-6">
+            <!-- Form Element sizes -->
+            <div class="box box-warning">
+              <div class="box-header with-border">
+                <h3 class="box-title">my by CAT</h3>
+              </div>
+              <div class="box-body">
+                <center><img src="/logo/my.jpg" width="200" height="80"></center>
+                <input class="form-control input-lg" type="text" placeholder="เบอร์โทร" ng-model="my_number">
+                <br>
+                <select class="form-control" ng-model="my_cash">
+                  <option value="">คลิกเลือกราคา</option>
+                  <option value="10">10</option>
+                  <option value="20">20</option>
+                  <option value="50">50</option>
+                  <option value="100">100</option>
+                  <option value="300">300</option>
+                  <option value="500">500</option>
+                </select>
+                <br>
+                <button type="button" class="btn btn-default btn-block" ng-click="topupRefill(5,my_number,my_cash,users)">เติมเงิน</button>
+              </div>
+
+              <!-- /.box-body -->
+            </div>
+            <!-- /.box -->
+          </div>
+
+          <div class="col-md-6">
+            <!-- Form Element sizes -->
+            <div class="box box-default">
+              <div class="box-header with-border">
+                <h3 class="box-title">I MOBILE 3G X</h3>
+              </div>
+              <div class="box-body">
+                <center><img src="/logo/imobile.jpg" width="200" height="80"></center>
+                <input class="form-control input-lg" type="text" placeholder="เบอร์โทร" ng-model="imobile_number">
+                <br>
+                <select class="form-control" ng-model="imobile_cash">
+                  <option value="">คลิกเลือกราคา</option>
+                  <option value="20">20</option>
+                  <option value="30">30</option>
+                  <option value="40">40</option>
+                  <option value="50">50</option>
+                  <option value="60">60</option>
+                  <option value="70">70</option>
+                  <option value="80">80</option>
+                  <option value="90">90</option>
+                  <option value="100">100</option>
+                  <option value="150">150</option>
+                  <option value="200">200</option>
+                  <option value="250">250</option>
+                  <option value="300">300</option>
+                  <option value="350">350</option>
+                  <option value="400">400</option>
+                  <option value="450">450</option>
+                  <option value="500">500</option>
+                  <option value="800">800</option>
+                  <option value="1000">1000</option>
+                </select>
+                <br>
+                <button type="button" class="btn btn-default btn-block" ng-click="topupRefill(4,imobile_number,imobile_cash,users)">เติมเงิน</button>
+              </div>
+
+              <!-- /.box-body -->
+            </div>
+            <!-- /.box -->
+          </div>
+
+          <div class="col-md-6">
+            <!-- Form Element sizes -->
+            <div class="box box-default">
+              <div class="box-header with-border">
+                <h3 class="box-title">PENGUIN</h3>
+              </div>
+              <div class="box-body">
+                <center><img src="/logo/penguinsim.jpg" width="200" height="80"></center>
+                <input class="form-control input-lg" type="text" placeholder="เบอร์โทร" ng-model="pengoin_number">
+                <br>
+                <select class="form-control" ng-model="pengoin_cash">
+                  <option value="">คลิกเลือกราคา</option>
+                  <option value="10">10</option>
+                  <option value="20">20</option>
+                  <option value="30">30</option>
+                  <option value="40">40</option>
+                  <option value="50">50</option>
+                  <option value="100">100</option>
+                  <option value="200">200</option>
+                  <option value="300">300</option>
+                  <option value="500">500</option>
+                </select>
+                <br>
+                <button type="button" class="btn btn-default btn-block" ng-click="topupRefill(7,pengoin_number,pengoin_cash,users)">เติมเงิน</button>
+              </div>
+
+              <!-- /.box-body -->
+            </div>
+            <!-- /.box -->
+          </div>
+
+        </div>
+
+        <!-- /.box -->
+      </section>
+      <!-- /.content -->
+    </div>
+    <!-- /.container -->
+  </div>
+  <!-- /.content-wrapper -->
+
+
+  <div class="modal fade" id="modal-topup-status">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span></button>
+          <h4 class="modal-title">ผลการเติมเงิน</h4>
+        </div>
+        <div class="modal-body text-center" ng-bind-html="content_topup_status">
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default btn-block" data-dismiss="modal">ปิด</button>
+        </div>
+      </div>
+      <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+  </div>
+  <!-- /.modal -->
+
+
+  <footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>Version</b> 1.0
+      </div>
+      <strong>Copyright &copy; 2017 <a href="#">NEW PHONE</a>.</strong> All rights
+      reserved.
+    </div>
+    <!-- /.container -->
+  </footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery 3 -->
+<script src="/bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- SlimScroll -->
+<script src="/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<!-- FastClick -->
+<script src="/bower_components/fastclick/lib/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="/dist/js/demo.js"></script>
+
+<script>
+
+    branch_name = '{{$user->branch_name}}';
+    var Encryption={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Encryption._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9+/=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Encryption._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/rn/g,"n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}};
+
+</script>
+</body>
+</html>
