80ada5ee20fdb6b87dd751383068dc99ff5c5b21
newnew01 <newnew01@gmail.com>
2017-12-20 01:54:17 +0700

    progress

--- routes/web.php
+++ routes/web.php
@@ -18,24 +18,33 @@ use Carbon\Carbon;
 Route::get('/topup/{token}', function ($token) {
     $shiftHour = 7;
     $token_data = base64_decode($token);
-    $splitData = explode('@', $token_data, 2);
-    $date_time = Carbon::now()->addHour($shiftHour);
+    if(substr( $token_data, 0, 4 ) === "aBcD"){
+        $token_data = substr( $token_data, 4, strlen($token_data) );
+        $splitData = explode('@', $token_data, 2);
+        $date_time = Carbon::now()->addHour($shiftHour);
+        $date_time_access = Carbon::createFromFormat('dmYhi', $splitData[0]);
+        if($date_time_access->addMinute(10) > $date_time){
+            //echo 'OK'.$date_time_access.'|'.$date_time;
 
-    $date_time_access = Carbon::createFromFormat('dmYhi', $splitData[0]);
-    if($date_time_access->addMinute(10) > $date_time){
-        //echo 'OK'.$date_time_access.'|'.$date_time;
-
-        $user = User::where('name','=',$splitData[1]);
-        if($user->exists()){
-            $user = $user->first();
-            return view('topup')->with(compact('user'));
+            $user = User::where('name','=',$splitData[1]);
+            if($user->exists()){
+                $user = $user->first();
+                return view('topup')->with(compact('user'));
+            }else{
+                echo 'ACCESS DENIED : UNAUTHORIZED';
+            }
         }else{
-            echo 'ACCESS DENIED : UNAUTHORIZED';
+            //echo 'NOT OK'.$date_time_access.'|'.$date_time;
+            echo 'ACCESS DENIED : TIMEOUT';
         }
     }else{
-        //echo 'NOT OK'.$date_time_access.'|'.$date_time;
-        echo 'ACCESS DENIED : TIMEOUT';
+        echo 'ACCESS DENIED : INVALID TOKEN STRING';
     }
+
+
+
+
+
     //dd($splitData[0].' '.$splitData[1]);
     //Carbon::now()->format(dmYhi);
     //return view('topup');
