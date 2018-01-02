ff45a63b992e15a446b386b4e3af7904dbc47ecf
newnew01 <newnew01@gmail.com>
2018-01-02 18:09:53 +0700

    add api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+
+
 use Illuminate\Http\Request;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -23,3 +28,98 @@ Route::post('/log/update/','LogController@updateLogStatus');
 Route::get('/log/today/','LogController@getTodayLog');
 Route::get('/log/today/report','LogController@getTodayReport');
 Route::get('/log/today/report-total','LogController@getTodayReportTotal');
+
+
+
+
+Route::post('/balance', function (Request $request) {
+    $username = $request->input('username');
+    $password = $request->input('password');
+
+    $client = new GuzzleHttp\Client();
+    $response = $client->request('POST', 'https://api.easysoft.in.th/topup/v2.php', [
+        'form_params' => [
+            'username' => $username,
+            'password' => $password,
+            'amount' => '1',
+        ]
+    ]);
+    $xml=new SimpleXMLElement($response->getBody()->getContents());
+    echo json_encode($xml);
+});
+
+Route::post('/network_status', function (Request $request) {
+    $username = $request->input('username');
+    $password = $request->input('password');
+
+    $client = new GuzzleHttp\Client();
+    $response = $client->request('POST', 'https://api.easysoft.in.th/topup/v2.php', [
+        'form_params' => [
+            'username' => $username,
+            'password' => $password,
+            'online' => '1',
+        ]
+    ]);
+    $xml=new SimpleXMLElement($response->getBody()->getContents());
+    echo json_encode($xml);
+});
+
+Route::post('/topup_status', function (Request $request) {
+    $username = $request->input('username');
+    $password = $request->input('password');
+    $orderid = $request->input('orderid');
+
+    $client = new GuzzleHttp\Client();
+    $response = $client->request('POST', 'https://api.easysoft.in.th/topup/v2.php', [
+        'form_params' => [
+            'username' => $username,
+            'password' => $password,
+            'mobile' => 'update',
+            'orderid' => $orderid
+        ]
+    ]);
+    $xml=new SimpleXMLElement($response->getBody()->getContents());
+    echo json_encode($xml);
+});
+
+Route::post('/cancel_topup', function (Request $request) {
+    $username = $request->input('username');
+    $password = $request->input('password');
+    $orderid = $request->input('orderid');
+
+    $client = new GuzzleHttp\Client();
+    $response = $client->request('POST', 'https://api.easysoft.in.th/topup/v2.php', [
+        'form_params' => [
+            'username' => $username,
+            'password' => $password,
+            'mobile' => 'return',
+            'orderid' => $orderid
+        ]
+    ]);
+    $xml=new SimpleXMLElement($response->getBody()->getContents());
+    echo json_encode($xml);
+});
+
+Route::post('/topup_refill', function (Request $request) {
+    $username = $request->input('username');
+    $password = $request->input('password');
+    $network = $request->input('network');
+    $number = $request->input('number');
+    $cash = $request->input('cash');
+    $users = $request->input('users');
+
+    $client = new GuzzleHttp\Client();
+    $response = $client->request('POST', 'https://api.easysoft.in.th/topup/v2.php', [
+        'form_params' => [
+            'username' => $username,
+            'password' => $password,
+            'mobile' => 'refill',
+            'network' => $network,
+            'number' => $number,
+            'cash' => $cash,
+            'users' => $users
+        ]
+    ]);
+    $xml=new SimpleXMLElement($response->getBody()->getContents());
+    echo json_encode($xml);
+});
