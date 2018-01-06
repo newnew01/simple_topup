25308e19238ed736f7d02aad4b6b34c01fba9721
newnew01 <newnew01@gmail.com>
2018-01-07 00:40:06 +0700

    111

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('notify:today-report')
-                  ->dailyAt('00:38');
+                  ->dailyAt('00:41');
     }
 
     /**
